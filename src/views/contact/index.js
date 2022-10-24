8584480b4444e451b70aed293f0440a3e264c6a9
codeeblacc <codeeblacc@gmail.com>
2022-10-25 00:56:32 +0100

    Build Home Layout

--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiMail } from "react-icons/fi";
 
 
 export default function Contact() {
@@ -15,9 +16,29 @@ export default function Contact() {
               <div className="heading">
                 Let's turn ideas into Finished Products
               </div>
+              <br />
+              <p className="smoke-text">
+                ccumulating over five(5) years of experience designing and developing software, I have developed a deep and strong understanding
+              </p>
             </div>
             <div className="large-2 medium-12 flex justify-content-end">
               <p className="smoke-text">@contact</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="row">
+            <div className="large-2 medium-12">
+              <p>Cotnact Info</p>
+            </div>
+            <div className="large-8 medium-12 left-text-small">
+              <div className="contact-card">
+                <FiMail />
+                jaysmoke.inbox@gmail.com
+              </div>
+            </div>
+            <div className="large-2 medium-12 flex justify-content-end">
+              <p className="smoke-text">handles</p>
             </div>
           </div>
         </div>
