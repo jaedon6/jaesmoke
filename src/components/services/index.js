b0eb8bbf95df5e03847af7a642137b4d2bb22f32
jaesmoke <jaysmoke.inbox@gmail.com>
2022-10-26 21:21:29 +0100

    Updated Services

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -5,30 +5,30 @@ export default function ServicesWidget() {
         <div className="heading">Concept Prototyping</div>
         <br />
         <p className="smoke-text">
-          Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far.
+          I organize design experiments from paper to digital to implement ideas into digital format.
         </p>
         <br />
         <div className="chips">
-          <div className="chip">Figma</div>
           <div className="chip">Adobe&nbsp;After&nbsp;Effects</div>
           <div className="chip">Webflow</div>
           <div className="chip">Sketch</div>
           <div className="chip">Adobe&nbsp;XD</div>
-          <div className="chip">Webflow</div>
+          <div className="chip">Invision</div>
+          <div className="chip">Framer</div>
         </div>
       </div>
       <div className="service">
         <div className="heading">Full Stack Web Developement</div>
         <br />
         <p className="smoke-text">
-          Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far.
+          Having the proficiency to create both client-side and server-side applications is second nature to me.
         </p>
         <br />
         <div className="chips">
           <div className="chip">Python</div>
           <div className="chip">Django</div>
           <div className="chip">Wordpress</div>
-          <div className="chip">HTML&nbsp;CSS&nbsp;Sass</div>
+          <div className="chip">&#183;HTML&nbsp;&#183;CSS&nbsp;&#183;Sass</div>
           <div className="chip">PHP</div>
           <div className="chip">React</div>
           <div className="chip">Angular&nbsp;JS</div>
@@ -39,48 +39,52 @@ export default function ServicesWidget() {
         <div className="heading">Android and IOS Development</div>
         <br />
         <p className="smoke-text">
-          Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far.
+          My skills in programming languages and mobile application tools allow me to create, test and develop mobile applications.
         </p>
         <br />
         <div className="chips">
-          <div className="chip">Figma</div>
-          <div className="chip">Adobe&nbsp;After&nbsp;Effects</div>
-          <div className="chip">Webflow</div>
-          <div className="chip">Sketch</div>
-          <div className="chip">Adobe&nbsp;XD</div>
-          <div className="chip">Webflow</div>
+          <div className="chip">Swift</div>
+          <div className="chip">Java</div>
+          <div className="chip">Kotlin</div>
+          <div className="chip">Dart</div>
+          <div className="chip">Flutter</div>
+          <div className="chip">Rust</div>
+          <div className="chip">Firebase</div>
+          <div className="chip">Android&nbsp;Studio</div>
         </div>
       </div>
       <div className="service">
         <div className="heading">Graphic Design</div>
         <br />
         <p className="smoke-text">
-          Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far.
+          I create everything from posters and billboards to packaging, logos, and marketing materials using visual concepts to communicate information.
         </p>
         <br />
         <div className="chips">
           <div className="chip">Figma</div>
-          <div className="chip">Adobe&nbsp;After&nbsp;Effects</div>
-          <div className="chip">Webflow</div>
-          <div className="chip">Sketch</div>
-          <div className="chip">Adobe&nbsp;XD</div>
-          <div className="chip">Webflow</div>
+          <div className="chip">Photoshop</div>
+          <div className="chip">Illustrator</div>
+          <div className="chip">After&nbsp;Effects</div>
+          <div className="chip">InDesign</div>
+          <div className="chip">Design Principles</div>
         </div>
       </div>
       <div className="service">
         <div className="heading">Scripting & Pentesting</div>
         <br />
         <p className="smoke-text">
-          Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far.
+          Having a concord and deep understanding of computers, I can identify and resolve security vulnerabilities in computer systems.
         </p>
         <br />
         <div className="chips">
-          <div className="chip">Figma</div>
-          <div className="chip">Adobe&nbsp;After&nbsp;Effects</div>
-          <div className="chip">Webflow</div>
-          <div className="chip">Sketch</div>
-          <div className="chip">Adobe&nbsp;XD</div>
-          <div className="chip">Webflow</div>
+          <div className="chip">Linux</div>
+          <div className="chip">Terminals</div>
+          <div className="chip">Python</div>
+          <div className="chip">CMD</div>
+          <div className="chip">Powershell</div>
+          <div className="chip">Java</div>
+          <div className="chip">Batch&nbsp;Script</div>
+          <div className="chip">C++</div>
         </div>
       </div>
     </div>
